a623b6c6d99f831aea4fb3b2c5d8564c5fce076d
Tariq Bailey <tariqbailey627@gmail.com>
2019-10-20 19:00:15 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -3,7 +3,7 @@
 #   "Sarah" => 6, 
 #   "Amanda" => 27
 # }
-require 'pry'
+
 def happy_birthday(birthday_kids)
  birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
@@ -15,7 +15,6 @@ def age_appropriate_birthday(birthday_kids)
     puts  "Happy Birthday #{kids_name}! You are now #{age} years old!"
   else
     puts "You are too old for this."
-    binding.pry
     end
   end
 end
